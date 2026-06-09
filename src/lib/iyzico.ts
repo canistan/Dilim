9cdb0703f6122236a717b5b68ed1f0d2fdbfbbe3
Şemsi Can Albayrak <semsicanalbayrak@gmail.com>
2026-06-09 22:05:25 +0300

    fix: add Iyzico sandbox keys

--- src/lib/iyzico.ts
+++ src/lib/iyzico.ts
@@ -2,7 +2,7 @@
 import Iyzipay from 'iyzipay';
 
 export const iyzipay = new Iyzipay({
-  apiKey: process.env.IYZICO_API_KEY || 'sandbox-api-key',
-  secretKey: process.env.IYZICO_SECRET_KEY || 'sandbox-secret-key',
+  apiKey: process.env.IYZICO_API_KEY || 'sandbox-pF6JPex6gGBfpAWiHeXPomDJVBEr4RCs',
+  secretKey: process.env.IYZICO_SECRET_KEY || 'sandbox-ARBM7yLcE2ndXcHwwY7nDtmJQOyz9WTJ',
   uri: process.env.IYZICO_URI || 'https://sandbox-api.iyzipay.com',
 });
