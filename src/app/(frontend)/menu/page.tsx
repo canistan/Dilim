926f059879ed2e829cb0d0361c44449d894bd071
Şemsi Can Albayrak <semsicanalbayrak@gmail.com>
2026-06-13 19:45:58 +0300

    refactor: move menu page to frontend route group and add Instagram link to header

--- src/app/(frontend)/menu/page.tsx
+++ src/app/(frontend)/menu/page.tsx
@@ -9,11 +9,11 @@ export default function FigmaMenuPage() {
   const figmaEmbedUrl = 'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FIR8eKNvKGTLeWbKiYUVkg4%2FDilim-Pastanesi%3Ftype%3Ddesign%26node-id%3D2222-895%26t%3DWAagkDaKqcfVsuuf-0%26scaling%3Dmin-zoom%26page-id%3D0%253A1%26starting-point-node-id%3D1%253A667'
 
   return (
-    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0, overflow: 'hidden', backgroundColor: '#1a1a1a' }}>
+    <div className="w-full min-h-[85vh] bg-[#1a1a1a] flex flex-col pt-20">
       <iframe
         src={figmaEmbedUrl}
         allowFullScreen
-        style={{ border: 'none', width: '100%', height: '100%' }}
+        className="w-full flex-1 border-none"
         title="Dilim Pastaneleri Menü"
       />
     </div>
