f54701e65b34fd3b0d106cba029058617dd0b961
Şemsi Can Albayrak <semsicanalbayrak@gmail.com>
2026-09-02 13:46:17 +0300

    refactor: update menu page layout to full-screen iframe and append log entries

--- src/app/(frontend)/menu/page.tsx
+++ src/app/(frontend)/menu/page.tsx
@@ -6,14 +6,24 @@ export const metadata: Metadata = {
 }
 
 export default function FigmaMenuPage() {
-  const figmaEmbedUrl = 'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FIR8eKNvKGTLeWbKiYUVkg4%2FDilim-Pastanesi%3Ftype%3Ddesign%26node-id%3D2222-895%26t%3DWAagkDaKqcfVsuuf-0%26scaling%3Dmin-zoom%26page-id%3D0%253A1%26starting-point-node-id%3D1%253A667'
+  const figmaEmbedUrl =
+    'https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FIR8eKNvKGTLeWbKiYUVkg4%2FDilim-Pastanesi%3Ftype%3Ddesign%26node-id%3D2222-895%26t%3DWAagkDaKqcfVsuuf-0%26scaling%3Dmin-zoom%26page-id%3D0%253A1%26starting-point-node-id%3D1%253A667'
 
   return (
-    <div className="w-full min-h-[85vh] bg-[#1a1a1a] flex flex-col pt-20">
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        margin: 0,
+        padding: 0,
+        overflow: 'hidden',
+        backgroundColor: '#1a1a1a',
+      }}
+    >
       <iframe
         src={figmaEmbedUrl}
         allowFullScreen
-        className="w-full flex-1 border-none"
+        style={{ border: 'none', width: '100%', height: '100%' }}
         title="Dilim Pastaneleri Menü"
       />
     </div>
